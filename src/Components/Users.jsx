48d2f39427966b52c349ca96aa94a67ffc251496
sifat26 <sifatict26@gmail.com>
2024-04-25 13:37:08 +0600

    Delet a single User

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,6 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
+    const handleDelet=(_id)=>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,
+    {
+        method:"DELETE"
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.deletedCount>0){
+                alert("User deleted successfully");
+    
+        }})
+
+    }
     const Users=useLoaderData()
     console.log(Users);
     return (
@@ -10,7 +25,7 @@ const Users = () => {
                 {
                     Users.map(user=>
                         <p key={user._id}>
-                            {user.name} : {user.email}
+                            {user.name} : {user.email} <button onClick={()=>handleDelet(user._id)}>X</button>
                         </p>
                     )
                 }
